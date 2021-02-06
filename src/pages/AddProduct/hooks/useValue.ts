70a999cff382e1533184efa204cd26fb6730b4cf
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-06 17:20:24 -0300

    improv: adiciona types ao useValue e extende sua funcionalidade

--- src/pages/AddProduct/hooks/useValue.ts
+++ src/pages/AddProduct/hooks/useValue.ts
@@ -1,15 +1,19 @@
 import { ChangeEvent, useState } from 'react';
 
-type IUseValue = [string, (event: ChangeEvent<HTMLInputElement>) => void];
+export type IValidation = [boolean, string];
+export type IValidate = (value: string, errorMessage: string) => IValidation;
+export type IUseValue = [string, (event: ChangeEvent<HTMLInputElement>) => void, IValidation];
 
-const useValue = (initialValue = ''): IUseValue => {
+const useValue = (initialValue = '', validate: IValidate, errorMessage: string): IUseValue => {
   const [value, setValue] = useState(initialValue);
+  const [validation, setValidation] = useState<IValidation>([true, '']);
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValidation(validate(event.currentTarget.value, errorMessage));
     setValue(event.currentTarget.value);
   }
 
-  return [value, handleOnChange];
+  return [value, handleOnChange, validation];
 }
 
 export default useValue;
