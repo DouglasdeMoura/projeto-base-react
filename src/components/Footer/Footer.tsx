9c612b5cc98c3844a5029f4d8ea42e8e1aa5838d
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-08 19:53:26 -0300

    chore: adiciona o componente que exibe o rodapé

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FooterWrapper } from './styled-components';
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      Exemplo de e-commerce com React
+    </FooterWrapper>
+  );
+}
+
+export default Footer;
